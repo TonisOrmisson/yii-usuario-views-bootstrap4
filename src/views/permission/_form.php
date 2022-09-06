79aa11e0e00e105aeb8be42c2694bd5703acce44
tonis ormisson <tonis@andmemasin.eu>
2022-09-06 15:49:56 +0300

    init bs4

---
+++ src/views/permission/_form.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the 2amigos/yii2-usuario project.
+ *
+ * (c) 2amigOS! <http://2amigos.us/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+
+/**
+ * @var $this            yii\web\View
+ * @var $model           Da\User\Model\Permission
+ * @var $unassignedItems string[]
+ */
+
+?>
+
+<?php $form = ActiveForm::begin(
+    [
+        'enableClientValidation' => false,
+        'enableAjaxValidation' => true,
+    ]
+) ?>
+
+<?= $form->field($model, 'name') ?>
+
+<?= $form->field($model, 'description') ?>
+
+
+<?= $form->field($model, 'rule')->widget(Select2::class, [
+    'bsVersion' => 4,
+    'data' => ArrayHelper::map(Yii::$app->getAuthManager()->getRules(), 'name', 'name'),
+    'options' => [
+        'placeholder' => Yii::t('usuario', 'Select rule...'),
+    ],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+])?>
+
+<?= $form->field($model, 'children')->widget(Select2::class, [
+    'bsVersion' => 4,
+    'data' => $unassignedItems,
+    'options' => [
+        'multiple' => true
+    ],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+])?>
+<?= Html::submitButton(Yii::t('usuario', 'Save'), ['class' => 'btn btn-success btn-block']) ?>
+
+<?php ActiveForm::end() ?>
---
+++ src/views/role/_form.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the 2amigos/yii2-usuario project.
+ *
+ * (c) 2amigOS! <http://2amigos.us/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+/**
+ * @var $this  yii\web\View
+ * @var $model \Da\User\Model\Role
+ */
+
+use Da\User\Helper\AuthHelper;
+use kartik\select2\Select2;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$unassignedItems = Yii::$container->get(AuthHelper::class)->getUnassignedItems($model);
+?>
+
+<?php $form = ActiveForm::begin(
+    [
+        'enableClientValidation' => false,
+        'enableAjaxValidation' => true,
+    ]
+) ?>
+
+<?= $form->field($model, 'name') ?>
+
+<?= $form->field($model, 'description') ?>
+
+<?= $form->field($model, 'rule')->widget(Select2::class, [
+    'data' => ArrayHelper::map(Yii::$app->getAuthManager()->getRules(), 'name', 'name'),
+    'options' => [
+        'placeholder' => Yii::t('usuario', 'Select rule...'),
+    ],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+])?>
+
+<?= $form->field($model, 'children')->widget(Select2::class, [
+    'data' => $unassignedItems,
+    'options' => [
+        'multiple' => true
+    ],
+    'pluginOptions' => [
+        'allowClear' => true
+    ],
+])?>
+
+<?= Html::submitButton(Yii::t('usuario', 'Save'), ['class' => 'btn btn-success btn-block']) ?>
+
+<?php ActiveForm::end() ?>
